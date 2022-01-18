0ed1037f3c50636f7f8f36e80ec10bf752bce6d4
home <home@WINDOWS-PQ21EPG>
2022-01-18 22:18:09 +0530

    Add project files.

---
+++ ConsoleApp9/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleApp9
+{
+    class Program
+    {
+        static void Increment(int i)
+        {
+            i = i + 1;
+        }
+        static void Main()
+        {
+            int x = 1;
+            Increment( x);
+            Console.WriteLine("The value of x is: " + x);
+        }
+    }
+}
